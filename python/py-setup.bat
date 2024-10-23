0d5f18cc5b3337544917a0668005b78a4b9bb431
xteam jack <xteam.jack@gmail.com>
2024-10-23 14:22:51 +0530

    Updates to python bat files

--- python/py-setup.bat
+++ python/py-setup.bat
@@ -1,17 +1,42 @@
-@echo off
+rem echo off
+echo Python environment setup tool
+echo -------------------------------
+echo.
 
-set DIR=D:\bin\python\3.9.13
+set pyVer=%1
 
-set PATH=%DIR%\;%DIR%\Scripts;%PATH%
-set PY_LIBS=%DIR%\Lib;%DIR%\Lib\site-packages
-set PY_PIP=%DIR%\Scripts
-set SKIP_VENV=1
-set PIP_INSTALLER_LOCATION=%DIR%\get-pip.py
-set TRANSFORMERS_CACHE=%DIR%\transformers-cache
+if "%pyVer%"== "" (
+    echo Expected python version as arg1
+    echo Alternatively setting version 3.10.6 as default
+    set pyVer=3.10.6
+) 
 
+echo Setting Python environment %pyVer%
+set verDir=%~dp0%pyVer%
+echo Python Dir %verDir%
+
+set PATH=%verDir%\;%verDir%\Scripts;%PATH%
+set PY_LIBS=%verDir%\Lib;%verDir%\Lib\site-packages
+set PY_PIP=%verDir%\Scripts
+set PIP_INSTALLER_LOCATION=%verDir%\get-pip.py
+
+rem uncomment import site line in pythonxxx.pth file
+
+copy get-pip.py %PIP_INSTALLER_LOCATION%
 python --version
 python %PIP_INSTALLER_LOCATION%
 python -m pip install --upgrade pip
 pip --version
 
 pip install virtualenv
+
+echo.
+echo Checking key applications
+python --version
+pip --version
+jupyter --version
+
+echo.
+echo Configure additional utilities
+echo ...poppler
+set PATH=D:\bin\poppler\24.08.0\Library\bin;%PATH%

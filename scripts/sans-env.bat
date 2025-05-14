39987a355b34123c8cc18de9c2817eca3e22855b
xteam.jack <xteam.jack@gmail.com>
2025-05-14 10:03:12 +0530

    sans-env updated

--- scripts/sans-env.bat
+++ scripts/sans-env.bat
@@ -18,19 +18,32 @@ set SANS_LOG_BASE=D:/wspc3/logs
 set SANS_STORE_BASE=D:/wspc3/store
 set SANS_TEMP_BASE=D:/wspc3/temp
 
-:: Additional configurations
-:: This is required to run python application in monorepo configuration
-set PYTHONPATH=%SANS_CODE_BASE%\python
+set SANS_SERVER_NS=http://localhost:3001
+set SANS_SERVER_CONFIG=http://localhost:3002
 
 :: Application specific configuration
 set DH_JWT_SECRET=s3cR3t!K3y@1234#AbCdEfGhIjKlMnOpQrStUvWxYz0987654321
 
+:: Node configuration
+set NODE_ENV=%SANS_ENV%
+set PATH=D:\bin\node\v22.15.0;%PATH%
+    
+:: Additional configurations
+:: This is required to run python application in monorepo configuration
+set PYTHONPATH=%SANS_CODE_BASE%\python
+
+:: Configure additional python tools
+
 set PATH=D:\bin\pandoc\3.6.3;C:\Program Files\Tesseract-OCR;D:\bin\ffmpeg\bin;%PATH%
 
 set | findstr "SANS"
+echo.
 set | findstr "DH"
-
 
 set PNPM_HOME=D:\bin\pnpm\10.6.4
 
 set PATH=%PNPM_HOME%;%PATH%
+
+
+echo.
+
